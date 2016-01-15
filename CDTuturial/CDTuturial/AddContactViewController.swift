00a5b581bdfca17eec7b6b19ab95805535aaa5ea
shinisaru <sinitsyn@avast.com>
2016-01-15 07:30:43 +0100

    Setup

---
+++ CDTuturial/CDTuturial/AddContactViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AddContactViewController.swift
+//  CDTuturial
+//
+//  Created by shinisaru on 15/01/16.
+//  Copyright © 2016 Avast. All rights reserved.
+//
+
+import UIKit
+
+class AddContactViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
